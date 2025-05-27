38fe9972d56cdb16b810f1833ed9be101c1d2b52
imsansanich <sashulia731@gmail.com>
2025-05-27 19:51:55 +0200

    Update busiest_weekday_by_waste.sql

--- queries/busiest_weekday_by_waste.sql
+++ queries/busiest_weekday_by_waste.sql
@@ -1,6 +1,7 @@
 SELECT dd.WeekdayName, AVG(ft.Waste) AS average_waste_on_weekday
 FROM facttrips ft
-JOIN dimdate dd ON ft.DateId = dd.DateId
+JOIN dimdate dd
+ON ft.DateId = dd.DateId
 GROUP BY dd.WeekdayName
 ORDER BY average_waste_on_weekday DESC
 LIMIT 1;
